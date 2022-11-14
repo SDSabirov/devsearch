96b5daf457bc34034866149569c486710e282b0c
SDSabirov <sabir.dev7@gmail.com>
2022-11-15 01:58:57 +0300

    search+pagination

---
+++ devsearch/static/js/main.js
@@ -0,0 +1,27 @@
+
+
+    let searchForm=document.getElementById('searchForm')
+    let pageLinks=document.getElementsByClassName('page-link')
+
+    //ensuring search form exists
+
+    if(searchForm){
+      for(let i=0;pageLinks.length>i; i++){
+
+        pageLinks[i].addEventListener('click',function(e){
+          e.preventDefault()
+          
+          //Get the data attribute
+          let page=this.dataset.page
+
+          //add hiden search input to form 
+          searchForm.innerHTML +=`<input value=${page} name = "page" hidden/>`
+
+          //submit form 
+
+          searchForm.submit()
+          
+
+        })
+      }
+    }
